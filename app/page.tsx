78fd6ae7816340a852e05ac901e11406d48c39ba
geoffreyfarel <geoffreyfarrel67@gmail.com>
2026-01-23 14:28:04 +0800

    feat: art

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import Navbar from './components/navbar';
 import Hero from './components/hero';
 import Cocktails from './components/cocktails';
 import About from './components/about';
+import Art from './components/art';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -16,6 +17,7 @@ export default function Home() {
       <Hero />
       <Cocktails />
       <About />
+      <Art />
     </div>
   );
 }
